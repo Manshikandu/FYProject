80cce10aaccec25b94b1395de3aad942d5ccc59e
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-05-21 10:18:11 +0545

    database connection

---
+++ backend/db/ConnectMongoDb.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const ConnectMongoDb = async () => {
+    try
+    {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB connected : ${conn.connection.host}`);
+    }
+    catch(error)
+    {
+        console.error(`Error connection to mongoDB : ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default ConnectMongoDb;
