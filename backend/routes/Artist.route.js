fb99dd857802409f166d10b0fd6696c2cf0e5a71
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-05-22 10:27:36 +0545

    get by id-added

--- backend/routes/Artist.route.js
+++ backend/routes/Artist.route.js
@@ -1,21 +1,9 @@
 import express from 'express';
 import Artist from '../models/Artist.model.js';
-import { CreateOrUpdateArtist } from '../controllers/Artist.controller.js';
+import { ArtistProfile, CreateOrUpdateArtist } from '../controllers/Artist.controller.js';
 const router = express.Router();
 
-router.post('/create', CreateOrUpdateArtist);
-
-router.get('/:id', async(req, res) => {
-    try{
-        const artist = await Artist.findById(req.params.id);
-        if(!artist)
-            return res.status(404).json({error: 'Not found'});
-        res.json(artist);
-    }
-    catch(error)
-    {
-        res.status(500).json({error: 'Fetch error'});
-    }
-});
+router.post('/create', CreateOrUpdateArtist);   //Create Artist Profile
+router.get('/:id', ArtistProfile);   //get Artist profile by ID.
 
 export default router;
