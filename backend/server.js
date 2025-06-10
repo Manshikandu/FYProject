e26f6ce554902bcc3951d2f369b7850ce08806b0
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-06-10 12:55:18 +0545

    removed-duplicate-authRoute

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ import artistRoutes from "./routes/Artist.route.js"
 
 import authRoutes from "./routes/auth.route.js";
 import bookingRoutes from "./routes/Artist.Booking.route.js";
-import authRoutes from "./routes/auth.route.js";
-
 
 dotenv.config();
 
@@ -20,9 +18,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/artist", artistRoutes);
-app.use("/api/auth", authRoutes);
-
-
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
 
