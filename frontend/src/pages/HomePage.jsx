ae32538cf51124bdefe155dafd16878e7588980b
Greetika <bajracharyagreetika1@gmail.com>
2025-06-19 13:20:23 +0545

    navbar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,33 @@
 
 import Slider from '../components/Slider';
 import CategorySection from '../components/CategorySection';
+// import NavBar from '../components/NavBar';
 
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
 
 const HomePage = () => {
+  const location = useLocation();
+
+useEffect(() => {
+  if (location.state?.scrollTo) {
+    const section = document.getElementById(location.state.scrollTo);
+    if (section) {
+      setTimeout(() => {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }, 100); // wait until page mounts
+    }
+  }
+}, [location]);
   return (
     <>
-    
+      
       <Slider />
       <CategorySection />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
