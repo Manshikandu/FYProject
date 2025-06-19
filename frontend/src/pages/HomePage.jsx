43995f24adbdbf9ad1c60dc89734746088de4467
manshikandu <manshikandu@gmail.com>
2025-06-19 13:35:00 +0545

    main to feature

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,15 +2,30 @@
 import Slider from '../components/Slider';
 import CategorySection from '../components/CategorySection';
 
-import Footer from '../components/Footer';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
 
 const HomePage = () => {
+  const location = useLocation();
+
+useEffect(() => {
+  if (location.state?.scrollTo) {
+    const section = document.getElementById(location.state.scrollTo);
+    if (section) {
+      setTimeout(() => {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }, 100); // wait until page mounts
+    }
+  }
+}, [location]);
   return (
     <>
-    
+      
       <Slider />
       <CategorySection />
-      <Footer />
+
     </>
   );
 };
