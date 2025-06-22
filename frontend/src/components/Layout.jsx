afc3146079e7eeea6a18793aa6966222fec20ee5
manshikandu <manshikandu@gmail.com>
2025-06-22 12:54:01 +0545

    components

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,26 @@
+// Layout.jsx---------------------------------------------
+
+// components/Layout.jsx
+//import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import { Outlet, useLocation } from 'react-router-dom';
+
+const Layout = () => {
+  const location = useLocation();
+
+  // Show footer only on home page
+  const showFooter = location.pathname === '/';
+
+  return (
+    <>
+      <Navbar />
+      <div className="pt-[80px] min-h-screen">
+        <Outlet />
+      </div>
+      {showFooter && <Footer />}
+    </>
+  );
+};
+
+ export default Layout;
