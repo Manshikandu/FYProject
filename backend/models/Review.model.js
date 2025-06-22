30608e4f98a38d863ef50cb4236657fe8fb9cc45
manshikandu <manshikandu@gmail.com>
2025-06-22 12:47:31 +0545

    models

---
+++ backend/models/Review.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+  bookingId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Booking",
+    required: true
+  },
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  artistId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: [true, "Rating is required"]
+  },
+  reviewText: {
+    type: String,
+    required: false
+  }
+}, {
+  timestamps: true
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;
