ceae4ef8fbf2a48c850f606dbf501c87a9a1c67d
Greetika <bajracharyagreetika1@gmail.com>
2025-06-30 10:07:43 +0545

    main to feature

--- backend/routes/Artist.route.js
+++ backend/routes/Artist.route.js
@@ -3,7 +3,8 @@ import Artist from '../models/Artist.model.js';
 import {UpdateArtist,
         ArtistProfile,
 
-        upload
+        upload,
+        getAllArtists
         //availability
        // Updateavailability
      } from '../controllers/Artist.controller.js';
@@ -13,12 +14,17 @@ const router = express.Router();
 router.patch("/profile/:id", UpdateArtist);
 
 
-router.get('/:id', ArtistProfile);   //get Artist profile by ID.
+router.get('/profile/:id', ArtistProfile);   //get Artist profile by ID.
+
+// router.get('/:id', ArtistProfile);
+
 
 router.post('/upload-media', upload);   //Artist to upload media
 
-//router.get('/:id/availability', availability);  //fetch availability
-//router.put('/:id/availability', Updateavailability);  //Update availability
+router.get("/artists", getAllArtists); // add this in your routes
+
+
+
 
 
 export default router;
